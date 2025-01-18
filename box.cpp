0d4a02ccb18db8efa9a86715da688e415f10d728
FTurci <turci@me.com>
2025-01-18 16:23:01 +0000

    box defined

---
+++ box.cpp
@@ -0,0 +1,22 @@
+#include "box.h"
+
+Box:: Box(double lx, double ly, double x0, double y0){
+
+    this->setSides(lx,ly);
+    this->setOrigin(x0, y0);
+}
+void Box::setOrigin(double x0, double y0){
+    this->x0 = x0;
+    this->y0 = y0;
+}
+void Box::setSides(double lx, double ly){
+    this->sidex = lx;
+    this->sidey = ly;
+}
+
+double Box::getSidex(){
+    return this->sidex;
+}
+double Box::getSidey(){
+    return sidey;
+}
---
+++ box.h
@@ -0,0 +1,22 @@
+#ifndef BOX_H
+#define BOX_H
+
+
+class Box {
+    private:
+        double sidex;
+        double sidey;
+        double x0;
+        double y0;
+
+    public:
+        Box(double lx, double ly, double x0, double y0);
+        void setSides(double lx, double ly);
+        void setOrigin(double x, double y);
+        double getSidex();
+        double getSidey();
+
+};
+
+
+#endif
